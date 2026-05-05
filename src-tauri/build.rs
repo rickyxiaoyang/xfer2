90858ba4350a5dab5129122c9dfc9865603ced6f
Ricky Yang <rickyxiaoyang@gmail.com>
2026-05-04 21:53:43 -0400

    fix: icon

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    // Re-run the build script (and re-bake icons into the binary) whenever
+    // any icon file changes. Cargo doesn't watch image files by default.
+    println!("cargo:rerun-if-changed=icons");
+    println!("cargo:rerun-if-changed=tauri.conf.json");
+
     tauri_build::build()
 }
